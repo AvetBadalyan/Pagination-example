d1135fb15f6c46f80d606d85a048797960b08cd6
AvetBadalyan <avetbadalyan@gmail.com>
2022-11-19 01:04:47 +0400

    feat: add prev and next buttons

--- src/App.js
+++ src/App.js
@@ -13,10 +13,30 @@ function App() {
       return;
     }
     setFollowers(data[page]);
-  }, [loading, page]);
+  }, [data, loading, page]);
 
   const handlePage = (index) => {
     setPage(index);
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage === data.length) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
   };
 
   return (
@@ -34,15 +54,21 @@ function App() {
         </div>
         {!loading && (
           <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
             {data.map((item, index) => (
               <button
                 key={index}
-                className={`page-btn ${index === page ? "active-btn": null}`}
+                className={`page-btn ${index === page ? "active-btn" : null}`}
                 onClick={() => handlePage(index)}
               >
                 {index + 1}
               </button>
             ))}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
           </div>
         )}
       </div>
